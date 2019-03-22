94f4c163d8d6dc807badc49f986887052d96574c
Eric Seitz <EricESeitz@ku.edu>
2019-03-22 14:14:33 -0500

    Initial Ex4 Admin home creation

--- Exercise3/CreatePosts.php
+++ Exercise3/CreatePosts.php
@@ -5,7 +5,7 @@ $post = $_POST["post"];  //number of user-defined item 1
 //include css sheet in backend html
 //echo "<link rel='stylesheet' href='style.css' />";
 
-echo "<h1>Thank you for your submission!</h1>";
+echo "<h1>Thank you for your post!</h1>";
 echo "Username: " . $username . "<br>";
 echo "Post: " . $post . "<br>";
 
