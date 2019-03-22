51cd9981471e333a7e410e2fc41cb722414f76a4
Eric Seitz <EricESeitz@ku.edu>
2019-03-22 14:05:22 -0500

    Created text area for post, basic validation w/o SQL

--- Exercise3/CreatePosts.php
+++ Exercise3/CreatePosts.php
@@ -1,11 +1,12 @@
 <?php
 $username = $_POST["username"];  //number of user-defined item 1
-$password = $_POST["password"];  //number of user-defined item 1
+$post = $_POST["post"];  //number of user-defined item 1
 
 //include css sheet in backend html
 //echo "<link rel='stylesheet' href='style.css' />";
 
 echo "<h1>Thank you for your submission!</h1>";
-echo "Customer password: " . $password . "<br>";
+echo "Username: " . $username . "<br>";
+echo "Post: " . $post . "<br>";
 
 ?>
