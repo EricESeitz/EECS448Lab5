ade504e2941ab8871335275753a1ca16b00544fa
Eric Seitz <EricESeitz@gmail.com>
2019-03-31 00:46:07 -0500

    Ex5. Table created with all users successful.

--- Exercise5/ViewUsers.php
+++ Exercise5/ViewUsers.php
@@ -17,13 +17,18 @@ if ($conn->connect_error) {
 
 //Return all rows in Users table
 $result = $conn->query("SELECT * FROM Users");
+//Create table
+echo "<h1> All Users </h1>";
+echo "<table border='1px solid black'>";
+echo "<td>User ID:</td>"; 	//create initial header row
 
 //from https://www.w3schools.com/php/php_mysql_select.asp
-
+//while loop of users
 while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["user_ID"] . "<br>";
+    echo "<tr><td>" . $row["User_ID"] . "</td></tr>"; 	//create initial header row
+    //echo "User_ID: " . $row["User_ID"] . "<br>";
 }
-
+echo "</table>";		//end of table
 
 echo "<br><br><button onclick='goBack()'>Go Back</button> 
 <script>
