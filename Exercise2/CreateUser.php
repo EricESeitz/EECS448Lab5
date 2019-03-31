0e19a64ae47a1b9d4957a3b212abf7f684181d43
Eric Seitz <EricESeitz@gmail.com>
2019-03-30 23:19:12 -0500

    Ex2: successful SQL query and adding unique usernames.

--- Exercise2/CreateUser.php
+++ Exercise2/CreateUser.php
@@ -4,6 +4,9 @@ $username = "e775s696";
 $password = "ricueH9e";
 $dbname = "e775s696";
 
+$Newusername = $_POST["username"];  //number of user-defined item 1
+$Newpassword = $_POST["password"];  //number of user-defined item 1
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -11,17 +14,23 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 
 //below needs editing
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
+$sql = "INSERT INTO Users (user_ID) VALUES ('$Newusername')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: username '" . $Newusername . "' taken.<br>Please go back and choose a new username.";
 }
+
+echo "<br><br><button onclick='goBack()'>Go Back</button> 
+<script>
+function goBack() {
+  window.history.back();
+}
+</script>";
 
 $conn->close();
 ?> 
