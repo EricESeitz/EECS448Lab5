346baa1232c7d940086c792bdd065c77772b61c9
Eric Seitz <EricESeitz@gmail.com>
2019-03-31 00:30:25 -0500

    Ex3. Successfully checks username, gives error is not found, inserts into Posts with unique post ID

--- Exercise3/CreatePosts.php
+++ Exercise3/CreatePosts.php
@@ -7,6 +7,68 @@ $post = $_POST["post"];  //number of user-defined item 1
 
 echo "<h1>Thank you for your post!</h1>";
 echo "Username: " . $username . "<br>";
-echo "Post: " . $post . "<br>";
+echo "Post: " . $post . "<br><br>";
 
+//from EX2
+$servername = "mysql.eecs.ku.edu";
+$dbusername = "e775s696";
+$password = "ricueH9e";
+$dbname = "e775s696";
+
+// Create connection
+$conn = new mysqli($servername, $dbusername, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+
+//check if username present
+//Query if username present in Users table
+$usernameResult = $conn->query("SELECT user_ID FROM Users WHERE user_ID='$username'");
+//Stores the number of rows returned in $usernameResult
+$usernameNumofRows = mysqli_num_rows($usernameResult);
+//echo "usernameNumofRows: " . $usernameNumofRows . "<br>";
+//If no rows returned, username not in Users. Quit.
+if ($usernameNumofRows < 1) {
+    echo "Error: User ID not found!";
+    echo "<br><br><button onclick='goBack()'>Go Back</button> 
+<script>
+function goBack() {
+  window.history.back();
+}
+</script>";
+    exit();
+}
+
+
+//Create post_ID from the author name, current number of rows in table, and length of post
+//Returns the number of rows
+$result = $conn->query("SELECT * FROM Posts");
+//Returns the number of rows in $result
+$numRows = mysqli_num_rows($result);
+//echo "num of rows: " . $numRows . "<br>";
+//Get length of post
+$postLength = strlen($post);
+//Combine
+$post_ID = $username . $numRows . $postLength;
+
+//echo "post_ID: " . $post_ID . "<br>";
+//sql to insert into posts
+$sql = "INSERT INTO Posts (post_ID, content, author_ID) VALUES ('$post_ID', '$post', '$username')";
+if ($conn->query($sql) === TRUE) {
+    echo "New post created successfully. You may go back to make another post.";
+}else{
+    echo "Error: " . $conn->error;
+}
+
+echo "<br><br><button onclick='goBack()'>Go Back</button> 
+<script>
+function goBack() {
+  window.history.back();
+}
+</script>";
+
+$conn->close();
 ?>
