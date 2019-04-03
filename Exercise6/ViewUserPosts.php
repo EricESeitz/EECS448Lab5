f203b486ae0fc556fe263a5aef31583c391f91ea
Eric Seitz <EricESeitz@gmail.com>
2019-04-03 14:54:17 -0500

    EX6: Finalization, posts correctly showing in table, dropdown working.

--- Exercise6/ViewUserPosts.php
+++ Exercise6/ViewUserPosts.php
@@ -21,17 +21,16 @@ if ($conn->connect_error) {
 // echo "Connected successfully";
 
 //Return all rows in Users table
-echo "Username: " . $username;
 $result = $conn->query("SELECT * FROM Posts WHERE author_id = '$username'");
 //Create table
-echo "<h1> All Users </h1>";
+echo "<h1> All Posts for user: " . $username . "</h1>";
 echo "<table border='1px solid black'>";
-echo "<td>Post ID:</td><td>Content:</td><td>User ID:</td>"; 	//create initial header row
+echo "<td>Post ID:</td><td>Content:</td>"; 	//create initial header row
 
 //from https://www.w3schools.com/php/php_mysql_select.asp
 //while loop of users
 while($row = $result->fetch_assoc()) {
-    echo "<tr><td>" . $row["post_ID"] . "</td><td>" . $row["content"] . "</td><td>" . $row["author_id"] . "</td></tr>"; 	//create initial header row
+    echo "<tr><td>" . $row["post_ID"] . "</td><td>" . $row["content"] . "</td></tr>"; 	//create initial header row
     //echo "User_ID: " . $row["User_ID"] . "<br>";
 }
 echo "</table>";		//end of table
